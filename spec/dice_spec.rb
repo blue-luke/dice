c78be2bc7bb7aeda26d737c783f1403fe2587ec6
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-13 18:02:44 +0000

    Fixed first text, also refactored it to check the range of the dice score. Now doing same for second test

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,8 +1,10 @@
 require 'dice'
 
 describe Dice do
-  xit 'rolls one die and shows score' do
-    expect(subject.roll).to be_a_kind_of(Integer)
+  it 'rolls one die and shows score' do
+    score = subject.roll(1)[0]
+    expect(score).to be >0
+    expect(score).to be <7
   end
   it 'rolls many dice and shows scores' do
     expect(subject.roll(4)[3]).to be_a_kind_of(Integer)
