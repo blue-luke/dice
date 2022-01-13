8dd8a60719c04748d4fe2753cc11725478193123
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-13 18:07:41 +0000

    Task complete

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,9 +2,9 @@ require 'dice'
 
 describe Dice do
   it 'rolls one die and shows score' do
-    score = subject.roll(1)[0]
-    expect(score).to be >0
-    expect(score).to be <7
+    score = subject.roll(1)
+    expect(score[0]).to be >0
+    expect(score[0]).to be <7
   end
   it 'rolls many dice and shows scores' do
     score = subject.roll(4)
